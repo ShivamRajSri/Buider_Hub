fa3e098a473f49eaf188e449ef3924f27e1ea93e
Shivam Raj Srivastav <shivamrajsrivastav38@gmail.com>
2025-07-04 20:58:44 +0530

    Update DB.js

--- Backend/DB.js
+++ Backend/DB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 function connectToDb() {
-    mongoose.connect('mongodb://localhost:27017/LifeGuide'
+    mongoose.connect('Your Mongodb address'
     ).then(() => {
         console.log('Connected to DB');
     }).catch(err => console.log(err));
